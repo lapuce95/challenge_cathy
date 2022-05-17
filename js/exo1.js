00a546e6b5e2682c14559d2cd66e878b08aa349f
lapuce95 <cathy.lopez95850@gmail.com>
2022-05-17 11:05:16 +0200

    exo1

--- js/exo1.js
+++ js/exo1.js
@@ -1,7 +1,13 @@
 /* === Exo1 ===
  *
- * 1. Demander à l'utilisateur de rentrer un premier nombre dans une boîte de dialogue.
- * 2. Demander à l'utilisateur de rentrer un 2e nombre dans une boîte de dialogue.
- * 3. Additionner les deux nombres.
- * 4. Afficher le résultat dans #result.
- */
+ * 1. Demander à l'utilisateur de rentrer un premier nombre dans une boîte de dialogue.*/
+var a = window.prompt("Entrez le premier nombre");
+/*
+/* 2. Demander à l'utilisateur de rentrer un 2e nombre dans une boîte de dialogue.*/
+var b = window.prompt("rentrezle deuxième nombre");
+/* 3. Additionner les deux nombres.*/
+var somme = parseInt(a) + parseInt(b);
+
+/* 4. Afficher le résultat dans #result. */
+let sommeElement = document.querySelector("#result");
+sommeElement.textContent = somme;
